5748024a9d514e02dcbe45d642edfe5ebe1f6ca0
tz <0x449556@skiff.com>
2023-07-11 11:15:49 +0800

    56

--- VerifySignature/EIP712/VerifySignature.sol
+++ VerifySignature/EIP712/VerifySignature.sol
@@ -2,10 +2,54 @@
 pragma solidity ^0.8.17;
 
 contract VerifySignature {
-    bytes32 constant EIP712Domin_TypeHash =
+    bytes32 private constant EIP712DOMIN_TYPEHASH =
         keccak256(
             "EIP712Domin(string name,string version,uint256 chainId,address verifyingContract)"
         );
 
-    bytes32 constant Approve_TypeHash
+    bytes32 private constant APPROVE_TYPEHASH =
+        keccak256("Approve(address spender,uint256 value)");
+
+    bytes32 private DOMIN_SEPERATOR;
+
+    address private owner;
+    uint public number;
+
+    constructor() {
+        DOMIN_SEPERATOR = keccak256(
+            abi.encode(
+                EIP712DOMIN_TYPEHASH,
+                keccak256(bytes("EIP712")),
+                keccak256(bytes("1")),
+                block.chainid,
+                address(this)
+            )
+        );
+        owner = msg.sender;
+    }
+
+    function permitApprove(uint value, bytes memory signature) external {
+        require(signature.length == 65, "INVALID SIGNATURE LENGTH");
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        assembly {
+            r := mload(add(signature, 0x20))
+            s := mload(add(signature, 0x40))
+            v := byte(0, mload(add(signature, 0x60)))
+        }
+
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMIN_SEPERATOR,
+                keccak256(abi.encode(APPROVE_TYPEHASH, address(this), value))
+            )
+        );
+
+        address signer = ecrecover(digest, v, r, s);
+
+        require(signer == msg.sender, "INVALID SIGNATURE");
+        number = value;
+    }
 }
